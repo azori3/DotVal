3212d35c79b831db63f5a17d7c4f57f118334f08
wadizaatour <zaatourwadi1@gmail.com>
2017-12-12 01:22:36 +0100

    Add files via upload

---
+++ ReportModele.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webpidev.Models
+{
+    public class ReportModele
+    {
+
+        public int Id { get; set; }
+
+
+        [StringLength(255)]
+        public string mail { get; set; }
+
+        [StringLength(255)]
+        public string mailTo { get; set; }
+
+        [StringLength(255)]
+        public string Sujet { get; set; }
+
+        public DateTime DateBanneBegin { get; set; }
+
+        public DateTime DateBanneEnd { get; set; }
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webpidev.Models
+{
+    public class Users
+    {
+        public string DTYPE { get; set; }
+
+        public int id { get; set; }
+ 
+        public string address { get; set; }
+
+     
+        public string mail { get; set; }
+ 
+        public string password { get; set; }
+
+   
+        public string region { get; set; }
+
+  
+        public string rpassword { get; set; }
+
+      
+        public string nameImage { get; set; }
+
+        public int? numTel { get; set; }
+ 
+        public string organsationName { get; set; }
+
+ 
+        public string type { get; set; }
+
+     
+        public string firstName { get; set; }
+
+        
+        public string gender { get; set; }
+ 
+        public byte[] image { get; set; }
+
+  
+        public string lastName { get; set; }
+
+     
+        public string numberAction { get; set; }
+ 
+        public byte[] preferences { get; set; }
+
+    }
+}
