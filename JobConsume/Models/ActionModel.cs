3589ead38fa3a02b114a55b86b69f53a89dd2586
Sanvesch <saief.bourjini@esprit.tn>
2017-12-12 01:52:30 +0100

    Add files via upload

---
+++ JobConsume/Models/ActionModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webapp.Models
+{
+    public class ActionModel
+    {
+        public int idAction { get; set; }
+        public DateTime? dateDebutAction { get; set; }
+
+      
+        public DateTime? dateFinAction { get; set; }
+
+       
+        public string discriptionAction { get; set; }
+
+       
+        public string imageAction { get; set; }
+
+       
+        public string titreAction { get; set; }
+
+        public int localisation { get; set; }
+
+        public int? user_id { get; set; }
+
+        public float lag { get; set; }
+        public float lat { get; set; }
+        public String urlImage { get; set; }
+    }
+}
---
+++ JobConsume/Models/MapModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webapp.Models
+{
+    public class MapModel
+    {
+
+
+        public int idMap { get; set; }
+        public float Lag { get; set; }
+
+        public float Lat { get; set; }
+
+    }
+}
